93c64582d89bee012380ecd7a05f374b67b8001c
killakilla4k <60495535+killakilla4k@users.noreply.github.com>
2020-07-29 01:28:28 -0400

    Create tuna.java

---
+++ tuna.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class tuna {
+	static apples applesObject = new apples();
+
+	private static String password;
+	private static String username;
+	
+	public static boolean accountCreation(boolean exist){
+		System.out.println("What is your username?");
+		Scanner usernameScan = new Scanner(System.in);
+		String username = usernameScan.next();
+		System.out.println("What is your password?");
+		Scanner passwordScan = new Scanner(System.in);
+		String password = passwordScan.next();
+		return exist = true;
+	}
+
+	public static void Verification(String usernamePotential, String passwordPotential) {
+	if(passwordPotential != password || usernamePotential != username) {
+		System.out.println("That username or password is incorrect, please try again");
+		//there is a method the IDE made for me so i can call signIn(); but idk what it is xd
+		applesObject.signIn();
+	}
+	else {
+		System.out.println("Welcome "+ username);
+	}
+	
+
+	
+}
+
+}
